ff8e9f1e4d8e70fd582ae2eb0da125a54329c238
Mariam Haider <mfh.haider@gmail.com>
2020-04-06 20:56:43 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,10 +17,10 @@ class BankAccount
   end 
   
   def valid
-    if self.balance > 0
-      self.status = "open"
+    if self.status == "open" && self.balance > 0
+      true
     else 
-      self.status = "closed"
+      false
     end 
   end 
   
