2da2e61bfe93826ffd32639578f392c9950f4347
Mariam Haider <mfh.haider@gmail.com>
2020-04-06 20:51:28 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,7 +8,12 @@ class BankAccount
     @status = "open"
   end 
   
-  def deposit
-    
+  def deposit(amount)
+    balance += amount
   end 
+  
+  def display_balance
+    return self.balance
+  end 
+  
 end
