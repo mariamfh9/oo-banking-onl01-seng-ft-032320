263e942dcf8bec94869949d4275911c219feba38
Mariam Haider <mfh.haider@gmail.com>
2020-04-06 21:34:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,13 +16,16 @@ class Transfer
     end 
   end 
   
-  def execute_transaction
-    if self.sender.balance < self.amount
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end 
-   
-  end 
+   def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
   
   def reverse_transfer
     if @status == "complete"
