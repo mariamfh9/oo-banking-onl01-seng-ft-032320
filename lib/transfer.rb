f3c87f775214a2deed7582d006f346f0fa6bdc1a
Mariam Haider <mfh.haider@gmail.com>
2020-04-06 21:32:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,11 +25,11 @@ class Transfer
   end 
   
   def reverse_transfer
-    if self.status == "complete"
-      self.sender.balance += self.amount
-      self.receiver.balance -= self.amount
-      self.status = "reversed"
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
   end 
-end 
   
 end
